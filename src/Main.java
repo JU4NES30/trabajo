2ba3071ec9fa917ab8a084f47a6890c0eeefe97f
unknown <juanesvj30@gmail.com>
2024-09-30 11:04:38 -0500

    practica de codiggo GIT

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Juan Esteban Villegas Jimenez\n");
+        System.out.println("juan.villegas-ji@uniminuto.edu.co\n");
+        System.out.println("cl 3a #1-56 \n");
+        System.out.println("Jugar cartas\n");
+
+    }
+}
